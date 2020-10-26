a1a35cc4a3f7a34e4f046bada97b0e607983133a
Juwon <lordjuey@gmail.com>
2020-10-26 19:18:00 +1100

    model backend added

---
+++ compoundingpharmacyapp/backend/models/formula.model.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const formulaSchema = new Schema({
+    formulaname: {type:String,required: true},
+    formulatype: {type:String, required:true},
+    strength: {type:String, required:true},
+    noteDate: {type:Date, required : true},
+    formulaNote: {type:String, required :true},
+},{
+    timestamps: true,
+});
+
+const Formula = mongoose.model('Formula', formulaSchema);
+
+module.exports = Formula;
